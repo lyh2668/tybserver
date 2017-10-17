99ed5fc879a5819609a6177365f3504decf41852
unknown <lyh2668@163.com>
2017-10-17 09:44:15 +0800

    add admin

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
-import users from './users'
 import express from 'express'
+import users from './users'
+import admin from './admin'
 const router = express()
 
 export default app => {
   app.use('/api', router)
 
   router.use('/users', users)
+  router.use('/admin', admin)
 }
